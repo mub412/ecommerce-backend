ad8d18d52130ea15af5514cfe706e85c68a42e0c
mub412 <mohi.uddin@durbar.live>
2025-09-05 00:14:07 +0600

    new commit

--- main.go
+++ main.go
@@ -23,10 +23,7 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 
 func createProducts(w http.ResponseWriter, r *http.Request) {
 	handleCors(w)
-	if r.Method == "OPTIONS" {
-		w.WriteHeader(200)
-		return
-	}
+	handlePreflightReq(w, r)
 	var newProduct Product
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newProduct)
@@ -45,7 +42,11 @@ func handleCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
 }
-
+func handlePreflightReq(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(200)
+	}
+}
 func sendData(w http.ResponseWriter, data interface{}, statusCode int) {
 	w.WriteHeader(statusCode)
 	encoder := json.NewEncoder(w)
