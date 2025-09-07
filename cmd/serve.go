e785621317292e5b909699cf254530f2432e8f5c
mub412 <mohi.uddin@durbar.live>
2025-09-08 02:01:41 +0600

    1:30:09

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,15 +3,24 @@ package cmd
 import (
 	"ecommerce/global_router"
 	"ecommerce/handlers"
+	"ecommerce/middleware"
 	"fmt"
 	"net/http"
 )
 
 func Serve() {
+	manager := middleware.NewManager()
+
 	mux := http.NewServeMux()
-	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProducts))
-	mux.Handle("GET /products/{productId}", http.HandlerFunc(handlers.GetProductByID))
-	mux.Handle("POST /products", http.HandlerFunc(handlers.CreateProducts))
+	m := manager.With(middleware.Hudai, middleware.Logger)
+	h := m(handlers.Test)
+	mux.Handle("GET /uddin", h)
+	mux.Handle("GET /mohi", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.Test))))
+
+	mux.Handle("GET /products", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.GetProducts))))
+	mux.Handle("GET /products/{productId}", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.GetProductByID))))
+	mux.Handle("POST /products", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.CreateProducts))))
+
 	globalRouter := global_router.GlobalRouter(mux)
 	fmt.Println("Server running on : 8080")
 	err := http.ListenAndServe(":8080", globalRouter)
